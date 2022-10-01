48c6e3767f0d25d9bc13c77f4bfc4da38ad7c29f
Kelsey <kearcher321@gmail.com>
2022-10-01 09:22:39 -0400

    Initial Commit

---
+++ Shared/BlackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackApp.swift
+//  Shared
+//
+//  Created by Kelsey Archer on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
